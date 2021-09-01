bf10bfbb6493491666d06c1f35b44f46c8e4a95a
Joris Langlois <joris.langlois@knplabs.com>
2021-09-01 17:55:09 +0300

    renamed the app component in page component

---
+++ src/components/page.js
@@ -0,0 +1,12 @@
+import "./page.scss";
+
+// Page :: Props -> React.Component
+const Page = () =>
+  <main>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+    <div className="square"></div>
+  </main>
+
+export default Page;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app";
+import Page from "./components/page";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Page />
   </React.StrictMode>,
   document.getElementById("root")
 );
